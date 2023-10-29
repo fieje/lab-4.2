4466742561f4cd7b35f59d946403cce282992ac7
Chukhrai <oleksandr.chukhrai@dspu.edu.ua>
2023-10-29 18:49:51 +0200

    first commit

---
+++ 4.2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+ double x, xp, xk, dx, A, B, y;
+ cout << "xp = "; cin >> xp;
+ cout << "xk = "; cin >> xk;
+ cout << "dx = "; cin >> dx;
+ cout << fixed;
+ cout << "---------------------------" << endl;
+ cout << "|" << setw(5) << "x" << " |"
+ << setw(7) << "y" << " |" << endl;
+ cout << "---------------------------" << endl;
+ x = xp;
+ while (x <= xk)
+ {
+ A = 2 * (fabs(x - 5));
+ if (x < -1)
+ B = (sin(x) * sin(x)) / (1 + fabs(cos(x)));
+ else
+ if (-1 <= x && x <= 1)
+ B = cos(1.0 / fabs(x + 2)) * cos(1.0 / fabs(x + 2));
+ else
+ B = log10(fabs(x + 2));
+ y = A - B ;
+ cout << "|" << setw(7) << setprecision(2) << x
+ << " |" << setw(10) << setprecision(3) << y
+ << " |" << endl;
+ x += dx;
+ }
+ cout << "---------------------------" << endl;
+ return 0;
+}
